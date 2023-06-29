7149a2973639812d70d7ea40544e961b58e0b687
María Belén Alegre <mariabelenalegre@gmail.com>
2023-06-29 16:30:25 -0300

    Actualizacion para 2da preentrega

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//PRE-ENTREGA 1
 alert("Bienvenidos a a DECO-HOME")
 alert("Las compras pueden ser realizadas por mayores de 18 años")
 
@@ -82,9 +83,179 @@ do {
 alert("el total del carrito es de $" + total);
 
 
+//PRE-ENTREGA 2
+
+//FOR...IN
+
+const Sabanas = { color: "blancas", tamaño: "king", hilos: 600 };
+
+
+for (const atributo in Sabanas) { 
+    console.log(atributo + ": " + Sabanas[atributo]);
+  }
+
+  
+  if ("calidad" in Sabanas){
+    console.log("el atributo calidad esta en sabanas");
+    } else {
+        console.log("el atributo calidad no esta en sabanas");
+        }
+
+
+
+        //CONSTRUCTORES
+
+function Producto (textil,color,propiedad,precio){ 
+    this.textil = textil;  
+    this.color = color;
+    this.hilos = propiedad;
+    this.precio = precio;
+    }
+    
+    const producto1 = new Producto("sabanas","blancas","600 Hilos",  10000);
+    
+    console.log(producto1)
+
+
+
+    //ARRAYS
+
+const arrayVacio = []; 
+
+const arrayTextiles = ["Sabanas", "Toallones", "Toallas de mano"];
+
+console.log (arrayVacio);
+console.log (arrayTextiles);
+
+
+console.log(arrayTextiles[0]);
+console.log(arrayTextiles[2]);
+
+arrayTextiles[2] = "funda";
+
+console.log(arrayTextiles)
+
+
+
+//ATRIBUTOS
+//LENGTH
+
+
+console.log("cantidad de textiles: " + arrayTextiles.length);
+
+const lista = ["Sabanas", "Toallones", "Toallas de mano"]; 
+
+for(let i = 0; i < lista.length; i ++){
+    console.log(lista[i]);
+}
+
+
+//METODOS:
+
+//PUSH
+
+const coloresSabanas = ["blanca", "celeste", "rosada"]
+console.log(coloresSabanas);
+
+let masColoresSabanas = "lila";
+
+coloresSabanas.push(masColoresSabanas);
+coloresSabanas.push("amarilla");
+console.log(coloresSabanas);
+
+//UNSHIFT
+
+coloresSabanas.unshift("verde");
+console.log(coloresSabanas);
+
+
+
+//CARRITO DE COMPRAS
+
+const carrito = [];
+
+function insertarTextilesEnCarrito(producto){
+    carrito.push(producto); 
+}
+
+function cargarTextilesEnCarrito() {
+    let seguir;
+    let producto;
+    do {
+producto = prompt("Ingrese textil deseado");
+insertarTextilesEnCarrito(producto)
+seguir = prompt("Desea continuar? si/no");
+    }while(seguir === "si");
+
+}
+
+function muestraCarrito() {
+   for (let i = 0 ; i < carrito.length; i++) {
+    console.log(carrito[i]);
+   } 
+}
+
+cargarTextilesEnCarrito();
+muestraCarrito();
+
+
+//SHIF
+
+let quitoTextil = carrito.shift();
+console.log("Quite el textil: " + quitoTextil);
+muestraCarrito();
+
+
+//POP
+
+let otrotextilQuitado = carrito.pop();
+console.log("Quite otro textil; " + otrotextilQuitado);
 
 
 
 
+
+/// METODOS DE BUSQUEDA Y TRANSFORMACION AVANZADA DE ARRAYS
+
+//SOME
+const textiles1 = [{tex:"sabanas", color: "celestes"}, {tex:"toalla", color: "blanca"}]
+
+if(textiles1.some((item) => {return item.tex === "sabanas"})) {
+    console.log("tex sabanas tienen descuentos");
+}else {
+    console.log("no tiene descuento");
+}
+
+//MAP
+const textiles1SinIVA = [{tex:"sabanas", color: "celestes", precio: 10560.52}, {tex:"toalla", color: "blanca", precio: 8520.89}]
+const textiles1ConIVA = textiles1SinIVA.map((item)=> {return {tex : item.tex, color : item.color, precio: item.precio*1.21}})
+
+console.log(textiles1SinIVA)
+console.log(textiles1ConIVA)
+
+//MATH
+//REDONDEO
+console.log("redondeo honesto sin IVA");
+textiles1SinIVA.forEach((item) => {
+    console.log(Math.round(item.precio))
+})
+
+console.log("redondeo honesto con IVA");
+textiles1ConIVA.forEach((item) => {
+    console.log(Math.round(item.precio))
+})
+
+//REDUCE
+const totalCarrito = textiles1ConIVA.reduce((total,item) => {return total + item.precio}, 0)
+console.log(totalCarrito);
+
+//SORT
+textiles1SinIVA.sort ((item1,item2)=> {return item2.precio - item1.precio});
+console.log(textiles1SinIVA)
+
+//CLASE DATE
+const fechaDeCompra = new Date(2023,3,25);
+console.log(fechaDeCompra);
+consolelog(fechaDeCompra.toLocaleString)
 
 
